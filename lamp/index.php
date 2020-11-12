c86dc113554d12f374ba48ebafd07986c991d588
ekaterinagp <katya.koriakina@gmail.com>
2020-11-12 08:21:45 +0100

    lamp setup with composer

---
+++ lamp/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="style.css">
+
+  <title>
+    FakeID</title>
+</head>
+
+<body>
+
+  <?php
+
+
+  ?>
+
+  <h2>Here comes FakeID in LAMP</h2>
+
+</body>
+
+</html>
