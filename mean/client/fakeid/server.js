eb6722f3669d57f02fa5d477e92493b5b0473f77
elisabetros <32452401+elisabetros@users.noreply.github.com>
2020-11-17 09:37:50 +0100

    environmant urls

--- mean/client/fakeid/server.js
+++ mean/client/fakeid/server.js
@@ -6,14 +6,4 @@ var app = express();
 var distDir = __dirname + "/dist/fakeid/";
 app.use(express.static(distDir));
 
-// Install express server 
-// const express = require('express');
-// const path = require('path');
-// const app = express();
-// // Serve only the static files form the dist directory
-// app.use(express.static(__dirname + '/dist/fakeid'));
-// app.get('/*', function(req, res) {
-//   res.sendFile(path.join(__dirname + '/dist/fakeid/index.html'));
-// });
-// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
