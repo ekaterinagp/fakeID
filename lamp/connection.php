ac99d865093d1c3e342c936f097a0fc04de01078
ekaterinagp <katya.koriakina@gmail.com>
2020-11-17 12:08:45 +0100

    connected and test

--- lamp/connection.php
+++ lamp/connection.php
@@ -1,24 +1,23 @@
 <?php
 
 
-if(getenv("CLEARDB_DATABASE_URL")){
+if (getenv("CLEARDB_DATABASE_URL")) {
     //deployment settings
     //Get Heroku ClearDB connection information
     $cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
     $host       = $cleardb_url["host"];
     $user       = $cleardb_url["user"];
     $password   = $cleardb_url["pass"];
-    $db         = substr($cleardb_url["path"],1);
-
-}else {
-    $host ='127.0.0.1';
+    $db         = substr($cleardb_url["path"], 1);
+} else {
+    $host = '127.0.0.1';
     $db = 'fakeid';
-    $username = 'root'; 
-    $password = ''; 
+    $user = 'root';
+    $password = '';
 }
-    $charset = 'utf8mb4';
-    
-    $dsn = "mysql:host=$host;dbname=$db;charset=$charset"; // using 'mysql:connecting to the host; specifying dbname';
+$charset = 'utf8mb4';
+
+$dsn = "mysql:host=$host;dbname=$db;charset=$charset"; // using 'mysql:connecting to the host; specifying dbname';
 
 $options = [
     PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
@@ -26,8 +25,8 @@ $options = [
     PDO::ATTR_EMULATE_PREPARES => false,
 ];
 
-try{
-    $connection = new PDO($dsn, $user, $password, $options);      
-} catch(PDOException $e){
+try {
+    $connection = new PDO($dsn, $user, $password, $options);
+} catch (PDOException $e) {
     throw new PDOException($e->getMessage(), (int)$e->getCode()); // throw me errors
 }
