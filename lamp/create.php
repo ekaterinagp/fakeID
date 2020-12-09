10555c13131fa652b37aebee28aea489145fe463
ekaterinagp <katya.koriakina@gmail.com>
2020-12-09 17:35:05 +0100

    change type date

--- lamp/create.php
+++ lamp/create.php
@@ -8,53 +8,53 @@ require_once(__DIR__ . '/../lamp/src/entity/sharedFunctions.php');
 require_once(__DIR__ . '/../lamp/components/menu.php');
 ?>
 
-  <main>
+<main>
 
-    <h2></h2>
-    <?php
-
-
-    $getFunction = new SharedFunctions();
-
-    ?>
-    <h2>Create User</h2>
-      <form method="POST" class="createForm">
-        <div class="form-field">
-          <input type="text" name="name" placeholder="Name">
-          <label for=""> Name</label>
-        </div>
-        <div class="form-field">
-          <select name="address_id" id="" placeholder="Select Address" required>
-            <option value="" disabled selected>Select Address</option>
-            <option value="1">Lygten 17</option>
-            <option value="2">Lygten 37</option>
-          </select>
-          <label for=""> Address </label>
-        </div>
-        <div class="form-field">
-          <input type="text" name="date_of_birth" placeholder="Date of Birth">
-          <label for=""> Date of Birth</label>
-        </div>
-        <div class="labelRadio">
-          <input type="radio" id="female" name="gender_value" value='0002'>
-          <label for="female"> Female </label>
-          <input id="male" type="radio" name="gender_value" value='0001'>
-          <label for="male"> Male </label>
-        </div>
-        <div class="labelRadio">
-          <input type="radio" id="employee" name="isEmployee" value='true'>
-          <label for="employee"> Is Employee </label>
-          <input type="radio" id="not" name="isEmployee" value='false'>
-          <label for="not"> Not Employee </label>
-
-        </div>
-        <input type="submit" onclick="submitCreateForm(event)">
-      </form>
+  <h2></h2>
+  <?php
 
 
-  </main>
-  <script src="./js/script.js"></script>
-  <script src="./js/create.js"></script>
+  $getFunction = new SharedFunctions();
+
+  ?>
+  <h2>Create User</h2>
+  <form method="POST" class="createForm">
+    <div class="form-field">
+      <input type="text" name="name" placeholder="Name">
+      <label for=""> Name</label>
+    </div>
+    <div class="form-field">
+      <select name="address_id" id="addressSelect" placeholder="Select Address" required>
+        <option value="" disabled selected>Select Address</option>
+        <option value="1">Lygten 17</option>
+        <option value="2">Lygten 37</option>
+      </select>
+      <label for=""> Address </label>
+    </div>
+    <div class="form-field">
+      <input type="date" name="date_of_birth" type="datetime-local" placeholder="Date of Birth">
+      <label for=""> Date of Birth</label>
+    </div>
+    <div class="labelRadio">
+      <input type="radio" id="female" name="gender_value" value='0002'>
+      <label for="female"> Female </label>
+      <input id="male" type="radio" name="gender_value" value='0001'>
+      <label for="male"> Male </label>
+    </div>
+    <div class="labelRadio">
+      <input type="radio" id="employee" name="isEmployee" value='true'>
+      <label for="employee"> Is Employee </label>
+      <input type="radio" id="not" name="isEmployee" value='false'>
+      <label for="not"> Not Employee </label>
+
+    </div>
+    <input type="submit" onclick="submitCreateForm(event)">
+  </form>
+
+
+</main>
+<script src="./js/script.js"></script>
+<script src="./js/create.js"></script>
 </body>
 
 </html>
--- lamp/js/create.js
+++ lamp/js/create.js
@@ -2,21 +2,50 @@
 
 async function submitCreateForm(event) {
   event.preventDefault();
+  let date = document
+    .querySelector("form")
+    .querySelector('input[name="date_of_birth"]').value;
+  let newDate = new Date(date);
+  console.log(date);
+  console.log(newDate);
+  var dd = String(newDate.getDate()).padStart(2, "0");
+  var mm = String(newDate.getMonth() + 1).padStart(2, "0");
+  var yy = newDate.getFullYear().toString().substr(2, 2);
+
+  newDate = dd + mm + yy;
+  newDate.toString();
+
   const formData = new FormData(document.querySelector("form"));
+
+  for (var pair of formData.entries()) {
+    if (pair[0] == "date_of_birth") {
+      pair[1] = newDate;
+    }
+  }
+
   let response = await fetch(path + "api/users", {
     method: "POST",
     body: formData,
   });
   console.log(response);
   response = await response.json();
-  if(response.status){
-    createNotification('success', 'User created')
+  if (response.status) {
+    createNotification("success", "User created");
     setTimeout(() => {
       window.location.href = path;
-    
-    }, 3000)
-  }else{
-    createNotification('error', response.message)
+    }, 3000);
+  } else {
+    createNotification("error", response.message);
   }
-  
 }
+
+// function changeformat() {
+//   document.querySelector('input[name="date_of_birth"]').value.replace(/-/g, "");
+//   console.log(document.querySelector('input[name="date_of_birth"]').value);
+// }
+
+// document
+//   .querySelector('input[name="date_of_birth"]')
+//   .addEventListener("mouseOut", () => {
+//     changeformat();
+//   });
