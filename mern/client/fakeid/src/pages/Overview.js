d314845e6ea393158cf45ab37a6234b7a1535041
elisabetros <32452401+elisabetros@users.noreply.github.com>
2020-12-11 12:09:45 +0100

    all users fetched

--- mern/client/fakeid/src/pages/Overview.js
+++ mern/client/fakeid/src/pages/Overview.js
@@ -1,31 +1,51 @@
 import React, { useEffect, useState } from 'react'
-
+import './overview.css'
 
 export default function Overview(){
+    const [ loading, setLoading ] = useState(true)
     const [ users, setUsers ] = useState()
 
 
-    const fetchUsers = async () => {
-        const response = await fetch('http://localhost:9090/users', {
-            method: 'GET'
-        })
-        console.log(response)
-    }
+    
 
     useEffect(() => {
         let isFetching = true
-        if(isFetching){
-            fetchUsers()
+        const fetchUsers = async () => {
+            const response = await fetch('http://localhost:9090/users')
+            const data = await response.json()
+            if(isFetching){
+                setUsers(data)
+                setLoading(false)
+            }
+            
+            console.log(users)
         }
-
+        fetchUsers()
         return () => isFetching =false
     }, [])
 
-    
+    if(loading){
+        return <div className="loader">LOADING</div>
+    }
+
 
     return(
-        <h2>
-            Overview
-        </h2>
+        <div>
+            <h2>Overview</h2>
+            <div className="usersContainer">
+                {users.map(user => {
+                  return (
+                      <div key={user._id} className="singleUser">
+                          <h3>{user.name}</h3>
+                            <p>adult or child</p>
+                            <p>{user.CVR? 'Employee' : ''}</p>
+                            <p>{user.maritalStatus}</p>
+                            <button>Edit</button>
+                      </div>
+                  )
+                })}
+            </div>
+        </div>
+        
     )
 }
