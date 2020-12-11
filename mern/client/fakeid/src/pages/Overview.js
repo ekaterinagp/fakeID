d3ec870e0861dca7345f29b6a2da6722ce4011a5
elisabetros <32452401+elisabetros@users.noreply.github.com>
2020-12-11 11:50:48 +0100

    cors and fetching

--- mern/client/fakeid/src/pages/Overview.js
+++ mern/client/fakeid/src/pages/Overview.js
@@ -1,7 +1,28 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 
 export default function Overview(){
+    const [ users, setUsers ] = useState()
+
+
+    const fetchUsers = async () => {
+        const response = await fetch('http://localhost:9090/users', {
+            method: 'GET'
+        })
+        console.log(response)
+    }
+
+    useEffect(() => {
+        let isFetching = true
+        if(isFetching){
+            fetchUsers()
+        }
+
+        return () => isFetching =false
+    }, [])
+
+    
+
     return(
         <h2>
             Overview
