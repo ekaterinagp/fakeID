e0c55920fccec463ad23cc3bad6c40b724549b35
elisabetros <32452401+elisabetros@users.noreply.github.com>
2020-12-21 12:17:50 +0100

    view for all users

---
+++ mean/client/fakeid/src/app/models/user.model.ts
@@ -0,0 +1,19 @@
+export class User {
+    constructor(
+        public id: string,
+        public name: string,
+        public dateOfBirth: string,
+        public age: number,
+        public address: string,
+        public genderIdentifation: string,
+        public gender: string,
+        public maritalStatus: string,
+        public CVR: string,
+        public companyName: string,
+        public spouse: object,
+        public child: object,
+        public parents: object,
+        
+
+    ) {}
+}
---
+++ mean/client/fakeid/src/app/pages/overview/overview.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+import { User } from '../../models/user.model';
+import { OverviewService } from '../../services/overview.service';
+
+@Component({
+  selector: 'app-overview',
+  templateUrl: './overview.component.html',
+  styleUrls: ['./overview.component.scss']
+})
+
+export class OverviewComponent implements OnInit {
+  users: User[] = [];
+
+  constructor( private overviewService: OverviewService) { }
+
+  ngOnInit(): void {
+    this.overviewService.getUsers()
+    .subscribe((users: any) => {this.users = users }) 
+  }
+
+}
