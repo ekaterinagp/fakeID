6f2f4385db37020cfe65a9a33fef72d83ce52d9b
elisabetros <32452401+elisabetros@users.noreply.github.com>
2020-12-22 13:08:03 +0100

    menu and material buttons in overview

--- mean/client/fakeid/src/app/app.module.ts
+++ mean/client/fakeid/src/app/app.module.ts
@@ -4,21 +4,26 @@ import { NgModule } from '@angular/core';
 
 import { MatToolbarModule } from  '@angular/material/toolbar';
 import { MatMenuModule } from '@angular/material/menu';
+import { MatButtonModule } from '@angular/material/button';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { OverviewComponent } from './pages/overview/overview.component';
+import { MenuComponent } from './components/menu/menu.component';
 
 @NgModule({
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
+    MatMenuModule,
+    MatButtonModule
   ],
   declarations: [
     AppComponent,
-    OverviewComponent
+    OverviewComponent,
+    MenuComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
