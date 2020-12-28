8c914146cf0400b1c52b723feca2b2ebc5595c06
ekaterinagp <katya.koriakina@gmail.com>
2020-12-28 12:54:09 +0100

    filters implemented

--- mean/client/fakeid/src/app/pages/overview/overview.component.ts
+++ mean/client/fakeid/src/app/pages/overview/overview.component.ts
@@ -1,8 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
-import { debounceTime } from "rxjs/operators";
 import { Subscription } from 'rxjs';
-
+import { debounceTime } from 'rxjs/operators';
 import { User } from '../../models/user.model';
 import { OverviewService } from '../../services/overview.service';
 
@@ -12,18 +11,28 @@ import { OverviewService } from '../../services/overview.service';
   styleUrls: ['./overview.component.scss'],
 })
 export class OverviewComponent implements OnInit {
-
-  
   users: User[] = [];
-  sortedUsers: User[] = []
+  sortedUsers: User[] = [];
   usersSub: Subscription = new Subscription();
-  searchSub = new Subscription;
-  searchText = new FormControl("");
+  searchSub = new Subscription();
+  searchText = new FormControl('');
   panelOpenState = false;
   sorters = {
     sortByName: false,
     sortByAge: false,
   };
+  isChild = new FormControl('');
+  gender = new FormControl('');
+  isCompany = new FormControl('');
+  filtersApplied = {
+    isChild: false,
+    isAdult: false,
+    isFemale: false,
+    isMale: false,
+    isCompany: false,
+    isNotCompany: false,
+  };
+  filtered: User[] = [];
 
   constructor(private overviewService: OverviewService) {}
 
@@ -39,69 +48,183 @@ export class OverviewComponent implements OnInit {
     });
 
     this.searchSub = this.searchText.valueChanges
-    .pipe(debounceTime(300))
-    .subscribe((res: string) => {
-      console.log(res)
-      this.sortedUsers = this.users.filter((user) =>
+      .pipe(debounceTime(300))
+      .subscribe((res: string) => {
+        console.log(res);
+        this.sortedUsers = this.users.filter((user) =>
           user.name.toLowerCase().includes(res.toLowerCase())
-      );
+        );
 
-      console.log(this.sortedUsers)
+        console.log(this.sortedUsers);
 
-      // if (this.filtered) {
+        // if (this.filtered) {
         // } else {
-          //   this.sortedUsers = this.users.filter((user) =>
-          //     user.commonName.toLowerCase().includes(res.toLowerCase())
-      //   );
-      //   console.log(this.sortedUsers);
-      // }
-    });
+        //   this.sortedUsers = this.users.filter((user) =>
+        //     user.commonName.toLowerCase().includes(res.toLowerCase())
+        //   );
+        //   console.log(this.sortedUsers);
+        // }
+      });
   }
 
   ngOnDestroy(): void {
     this.usersSub.unsubscribe();
-    this.searchSub.unsubscribe
+    this.searchSub.unsubscribe;
   }
 
-  changeSorters(sortBy: string):void {
-      if(sortBy == 'sortByAge'){
-      this.sorters.sortByAge = !this.sorters.sortByAge
-      this.sorters.sortByName = false
+  changeSorters(sortBy: string): void {
+    if (sortBy == 'sortByAge') {
+      this.sorters.sortByAge = !this.sorters.sortByAge;
+      this.sorters.sortByName = false;
     }
-    if(sortBy == 'sortByName'){
-      this.sorters.sortByName = !this.sorters.sortByName
-      this.sorters.sortByAge = false
-    }    
+    if (sortBy == 'sortByName') {
+      this.sorters.sortByName = !this.sorters.sortByName;
+      this.sorters.sortByAge = false;
+    }
   }
 
-  sortUsers( sortBy:string) :void{
+  sortUsers(sortBy: string): void {
     this.changeSorters(sortBy);
-    if(this.sorters.sortByAge){
-      this.sortUsersByAge()
+    if (this.sorters.sortByAge) {
+      this.sortUsersByAge();
     }
-    if(this.sorters.sortByName){
-      this.sortUsersByName()
+    if (this.sorters.sortByName) {
+      this.sortUsersByName();
     }
   }
-  
+
   sortUsersByName() {
-      this.sortedUsers.sort((a, b) => {
-        if (a.name.toLowerCase() > b.name.toLowerCase()) {
-          return 1;
-        }  
-        if (a.name.toLowerCase() < b.name.toLowerCase()) {
-          return -1;
-        }
-  
-        return 0;
-      });
-    }
- 
+    this.sortedUsers.sort((a, b) => {
+      if (a.name.toLowerCase() > b.name.toLowerCase()) {
+        return 1;
+      }
+      if (a.name.toLowerCase() < b.name.toLowerCase()) {
+        return -1;
+      }
 
-    sortUsersByAge() {
-    this.sortedUsers.sort((a,b) => {
-      return a.age - b.age;
-    })
+      return 0;
+    });
   }
 
+  sortUsersByAge() {
+    this.sortedUsers.sort((a, b) => {
+      return a.age - b.age;
+    });
+  }
+
+  showChildren() {
+    this.filtersApplied.isChild = true;
+    this.filtersApplied.isAdult = false;
+
+    this.applySelectedFilters();
+  }
+
+  showAdults() {
+    this.filtersApplied.isAdult = true;
+    this.filtersApplied.isChild = false;
+    this.applySelectedFilters();
+  }
+  showCompany() {
+    this.filtersApplied.isCompany = true;
+    this.filtersApplied.isNotCompany = false;
+
+    this.applySelectedFilters();
+  }
+
+  showNotCompany() {
+    this.filtersApplied.isNotCompany = true;
+    this.filtersApplied.isCompany = false;
+    this.applySelectedFilters();
+  }
+
+  isFemale(user: User) {
+    return user.gender == 'female';
+  }
+
+  isMale(user: User) {
+    return user.gender == 'male';
+  }
+
+  showFemale() {
+    this.filtersApplied.isFemale = true;
+    this.filtersApplied.isMale = false;
+    this.applySelectedFilters();
+  }
+
+  showMale() {
+    this.filtersApplied.isMale = true;
+    this.filtersApplied.isFemale = false;
+    this.applySelectedFilters();
+  }
+
+  showAll(filterType: string) {
+    if (filterType == 'gender') {
+      this.filtersApplied.isFemale = false;
+      this.filtersApplied.isMale = false;
+    }
+
+    if (filterType == 'company') {
+      this.filtersApplied.isCompany = false;
+      this.filtersApplied.isNotCompany = false;
+    }
+
+    if (filterType == 'child') {
+      this.filtersApplied.isChild = false;
+      this.filtersApplied.isAdult = false;
+    }
+
+    this.applySelectedFilters();
+  }
+
+  resetFilters() {
+    this.sortedUsers = this.users;
+    this.isChild.setValue('all');
+    this.isCompany.setValue('all');
+    this.gender.setValue('all');
+    this.filtersApplied = {
+      isChild: false,
+      isAdult: false,
+      isFemale: false,
+      isMale: false,
+      isCompany: false,
+      isNotCompany: false,
+    };
+  }
+  applySelectedFilters() {
+    console.log(this.filtersApplied);
+    this.sortedUsers = this.users;
+
+    if (this.filtersApplied.isNotCompany) {
+      this.filtered = this.sortedUsers.filter((user) => !user.CVR);
+      this.sortedUsers = this.filtered;
+    }
+
+    if (this.filtersApplied.isAdult) {
+      this.filtered = this.sortedUsers.filter((user) => !user.isChild);
+      this.sortedUsers = this.filtered;
+    }
+
+    if (this.filtersApplied.isChild) {
+      this.filtered = this.sortedUsers.filter((user) => user.isChild);
+
+      this.sortedUsers = this.filtered;
+    }
+
+    if (this.filtersApplied.isMale) {
+      this.filtered = this.sortedUsers.filter(this.isMale);
+      this.sortedUsers = this.filtered;
+    }
+
+    if (this.filtersApplied.isFemale) {
+      // this.showFemale();
+      this.filtered = this.sortedUsers.filter(this.isFemale);
+
+      this.sortedUsers = this.filtered;
+    }
+
+    if (this.filtersApplied.isCompany) {
+      this.filtered = this.sortedUsers.filter((user) => user.CVR);
+      this.sortedUsers = this.filtered;
+    }
+  }
 }
