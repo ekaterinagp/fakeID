55cd99f6741b31d92aeaa94764a0b1533260006d
ekaterinagp <katya.koriakina@gmail.com>
2020-12-28 14:47:33 +0100

    report created

--- mean/client/fakeid/cypress/plugins/index.js
+++ mean/client/fakeid/cypress/plugins/index.js
@@ -1,6 +1,19 @@
+// module.exports = (on, config) => {
+//   require("@cypress/code-coverage/task")(on, config);
+//   // IMPORTANT to return the config object
+//   // with the any changed environment variables
+//   return config;
+// };
+
+/**
+ * @type {Cypress.PluginConfig}
+ */
 const cypressTypeScriptPreprocessor = require("./cy-ts-preprocessor");
+const registerCodeCoverageTasks = require("@cypress/code-coverage/task");
+
 module.exports = (on, config) => {
   on("file:preprocessor", cypressTypeScriptPreprocessor);
-  require("@cypress/code-coverage/task")(on, config);
-  return config;
+
+  // enable code coverage collection
+  return registerCodeCoverageTasks(on, config);
 };
