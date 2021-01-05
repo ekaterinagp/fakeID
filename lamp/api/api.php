5be7feaa70b9f0b9e40c2b00b907ee7549f37af8
elisabetros <32452401+elisabetros@users.noreply.github.com>
2021-01-05 13:11:17 +0100

    cors

--- lamp/api/api.php
+++ lamp/api/api.php
@@ -10,7 +10,7 @@ header('Access-Control-Allow-Origin: *');
 
 header('Access-Control-Allow-Methods: GET, POST');
 
-// header("Access-Control-Allow-Headers: X-Requested-With");
+header("Access-Control-Allow-Headers: X-Requested-With");
 
 header('Content-Type: application/json');
 
