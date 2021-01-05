1f97950b40dc816880f3ccf3a4efac6824bb9062
elisabetros <32452401+elisabetros@users.noreply.github.com>
2021-01-05 16:14:54 +0100

    server fixed

--- mean/client/fakeid/server.js
+++ mean/client/fakeid/server.js
@@ -1,13 +1,14 @@
 var express = require("express");
+const path = require('path');
 
 var app = express();
 
 // Create link to Angular build directory
 var distDir = __dirname + "/dist/fakeid/";
 app.use(express.static(distDir));
+
 app.get('/*', function(req, res) {
-    res.sendFile('index.html', {root: distDir}
-  );
+    res.sendFile(path.join(__dirname + '/dist/index.html'));
   });
 
 app.listen(process.env.PORT || 8080);
