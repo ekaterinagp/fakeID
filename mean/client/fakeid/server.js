8ff74b9b596faedef75d21ec1804ec9429e9e4c4
elisabetros <32452401+elisabetros@users.noreply.github.com>
2021-01-05 16:10:54 +0100

    server test deployment angular

--- mean/client/fakeid/server.js
+++ mean/client/fakeid/server.js
@@ -5,5 +5,8 @@ var app = express();
 // Create link to Angular build directory
 var distDir = __dirname + "/dist/fakeid/";
 app.use(express.static(distDir));
-
+app.get('/*', function(req, res) {
+    res.sendFile('index.html', {root: 'dist/<name-on-package.json>/'}
+  );
+  });
 app.listen(process.env.PORT || 8080);
