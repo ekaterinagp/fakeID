adcf29b777728c6dd3b27b0c53a94c4584749afb
elisabetros <32452401+elisabetros@users.noreply.github.com>
2021-01-05 12:10:53 +0100

    testpage test

--- testpage/index.php
+++ testpage/index.php
@@ -1,1 +1,41 @@
-<?php include_once('home.html”'); ?>
+<?php 
+if($_SERVER["REMOTE_ADDR"]=="127.0.0.1"){
+    $local = True;
+    }else{
+    
+        $local = False;
+    }
+
+// include_once('home.html'); 
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>FakeID test page</title>
+</head>
+<body class="testpage">
+
+    <a href=
+    <?php
+        if($local){ echo "/fakeid/lamp/";}else{ echo 'https://fakeid-lamp.herokuapp.com/'; };
+    ?>
+    >LAMP</a>
+    <a href=
+    <?php
+        if($local){ echo "http://localhost:3000";}else{ echo 'https://fakeid-mern.herokuapp.com/'; };
+    ?>
+    >MERN</a>
+    <a href=
+    <?php
+        if($local){ echo "http://localhost:4200";}else{ echo 'https://fakeid-mean.herokuapp.com/'; };
+    ?>
+    >MEAN</button>
+    
+</body>
+</html>
