34e3278f65de428e4537bda589146ddcf56850e9
elisabetros <32452401+elisabetros@users.noreply.github.com>
2021-01-05 12:31:00 +0100

    header php

--- lamp/api/api.php
+++ lamp/api/api.php
@@ -6,7 +6,11 @@ $sharedFunctions = new SharedFunctions();
 
 $request_method = $_SERVER["REQUEST_METHOD"];
 
+header('Access-Control-Allow-Origin: *');
 
+header('Access-Control-Allow-Methods: GET, POST');
+
+header("Access-Control-Allow-Headers: X-Requested-With");
 
 header('Content-Type: application/json');
 
