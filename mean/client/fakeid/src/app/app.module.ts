5ca052200c4714da0b8acb441f1988a7414f7967
ekaterinagp <katya.koriakina@gmail.com>
2021-01-06 17:29:24 +0100

    moment removed, bundle size decreased

--- mean/client/fakeid/src/app/app.module.ts
+++ mean/client/fakeid/src/app/app.module.ts
@@ -1,11 +1,9 @@
 import { HttpClientModule } from '@angular/common/http';
 import { LOCALE_ID, NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { MomentDateAdapter } from '@angular/material-moment-adapter';
 import { MatButtonModule } from '@angular/material/button';
 import { MatButtonToggleModule } from '@angular/material/button-toggle';
 import {
-  DateAdapter,
   MatNativeDateModule,
   MAT_DATE_FORMATS,
   MAT_DATE_LOCALE,
@@ -21,8 +19,6 @@ import { MatSelectModule } from '@angular/material/select';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-// import * as moment from 'moment';
-import 'moment/locale/da';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './components/menu/menu.component';
@@ -30,18 +26,6 @@ import { NotificationComponent } from './components/notification/notification.co
 import { CreateUserComponent } from './pages/create-user/create-user.component';
 import { EditUserComponent } from './pages/edit-user/edit-user.component';
 import { OverviewComponent } from './pages/overview/overview.component';
-
-// moment.updateLocale('da', {
-//   longDateFormat: {
-//     LT: 'HH:mm',
-//     LTS: 'HH:mm:ss',
-//     L: 'DD-MM-YYYY',
-//     l: 'DD-MM-YYYY',
-//     LL: 'D-MMMM-YYYY',
-//     LLL: 'D-MMMM-YYYY HH:mm',
-//     LLLL: 'dddd [d.] D. MMMM YYYY [kl.] HH:mm',
-//   },
-// });
 
 const CUSTOM_DATE_FORMAT = {
   parse: {
@@ -88,7 +72,6 @@ const CUSTOM_DATE_FORMAT = {
       { provide: MAT_DATE_LOCALE, useValue: 'da-DK' },
       { provide: LOCALE_ID, useValue: 'da-DK' },
       { provide: MAT_DATE_FORMATS, useValue: CUSTOM_DATE_FORMAT },
-      { provide: DateAdapter, useClass: MomentDateAdapter },
     ],
     MatDatepickerModule,
     MatNativeDateModule,
